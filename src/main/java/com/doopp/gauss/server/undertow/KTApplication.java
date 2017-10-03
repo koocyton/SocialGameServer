3f5c38514346d7e8bfd3779b6e951f83a1916550
liuyi <liuyi@ushow.media>
2017-10-03 11:58:07 +0800

    打出的 jar 包，解压后能执行，jar 包不能执行，成功还差半步

--- src/main/java/com/doopp/gauss/server/undertow/KTApplication.java
+++ src/main/java/com/doopp/gauss/server/undertow/KTApplication.java
@@ -1,19 +1,21 @@
 package com.doopp.gauss.server.undertow;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class KTApplication {
 
     public static void main(String[] args) {
 
+        // 有问题，找不到模版
+        // final ApplicationContext context = new FileSystemXmlApplicationContext("classpath*:config/spring-undertow.xml");
+        // context.getBean("undertowServer");
+
         final ApplicationContext context = new ClassPathXmlApplicationContext("classpath:config/spring-undertow.xml");
-
-        // final ApplicationContext context = new ClassPathXmlApplicationContext("file:" + appPath + "/config/rootContext.xml");
-
-        // AbstractApplicationContext context = new ClassPathXmlApplicationContext("config/spring-undertow.xml");
-
-        // UndertowServer undertowServlet  =  (UndertowServer) context.getBean("undertowServlet");
+        // context.getEnvironment();
+        // UndertowServer undertowServer  =  (UndertowServer) context.getBean("undertowServer");
 
         // context.close();
     }
