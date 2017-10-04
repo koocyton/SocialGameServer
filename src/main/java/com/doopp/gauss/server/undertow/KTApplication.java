7592021fdfb167c0d637eb7955cf9a2965213d8c
liuyi <liuyi@ushow.media>
2017-10-04 22:15:34 +0800

    使用关闭钩子shutdownHook来进行销毁Bean

--- src/main/java/com/doopp/gauss/server/undertow/KTApplication.java
+++ src/main/java/com/doopp/gauss/server/undertow/KTApplication.java
@@ -2,6 +2,7 @@ package com.doopp.gauss.server.undertow;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
@@ -10,13 +11,16 @@ public class KTApplication {
     public static void main(String[] args) {
 
         // 有问题，找不到模版
-        final ApplicationContext context = new FileSystemXmlApplicationContext("classpath:config/spring-undertow.xml");
-        // context.getBean("undertowServer");
+        // final AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("classpath*:config/spring-undertow.xml");
+        // final ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:config/spring-undertow.xml");
+        final AbstractApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:config/spring-undertow.xml");
 
-        // final ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config/spring-undertow.xml");
-        // context.getEnvironment();
-        // UndertowServer undertowServer  =  (UndertowServer) context.getBean("undertowServer");
+        // add a shutdown hook for the above context...
+        // 使用关闭钩子shutdownHook来进行销毁Bean
+        ctx.registerShutdownHook();
 
-        // context.close();
+        // ctx.getEnvironment();
+        // UndertowServer undertowServer  =  (UndertowServer) ctx.getBean("undertowServer");
+        // ctx.close();
     }
 }
