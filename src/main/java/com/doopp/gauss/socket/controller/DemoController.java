097ac6632ccf8f564ce19ae7aea38a052597d024
liuyi <liuyi@ushow.media>
2017-10-06 20:50:07 +0800

    增加判断移动客户端的页面显示

--- src/main/java/com/doopp/gauss/socket/controller/DemoController.java
+++ src/main/java/com/doopp/gauss/socket/controller/DemoController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class DemoController {
 
@@ -20,13 +22,25 @@ public class DemoController {
     }
 
     @RequestMapping(value = "/chat-room")
-    public String chatRoom() {
-        // return new ModelAndView("socket/demo/chat_room");
-        return "socket/demo/chat_room";
+    public String chatRoom(HttpServletRequest request) {
+        return isMobileClient(request) ? "socket/demo/chat_room" : "socket/demo/mobile_chat_room";
     }
 
-    @RequestMapping(value = "/mchat-room")
-    public String mobileChatRoom() {
-        return "socket/demo/mobile_chat_room";
+    private boolean isMobileClient(HttpServletRequest request) {
+        String ua = request.getHeader("User-Agent");
+        String[] agent = { "Android", "iPhone", "iPod", "iPad", "Windows Phone", "MQQBrowser" };
+        boolean flag = false;
+        if (!ua.contains("Windows NT") || (ua.contains("Windows NT") && ua.contains("compatible; MSIE 9.0;"))) {
+            // 排除 苹果桌面系统
+            if (!ua.contains("Windows NT") && !ua.contains("Macintosh")) {
+                for (String item : agent) {
+                    if (ua.contains(item)) {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return flag;
     }
 }
