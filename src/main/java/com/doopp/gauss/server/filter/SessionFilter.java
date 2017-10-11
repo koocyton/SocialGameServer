94f2382ef0c31931d3e90b126d7229f12c423805
liuyi <liuyi@ushow.media>
2017-10-11 17:05:49 +0800

    webjars 没搞定

--- src/main/java/com/doopp/gauss/server/filter/SessionFilter.java
+++ src/main/java/com/doopp/gauss/server/filter/SessionFilter.java
@@ -57,6 +57,7 @@ public class SessionFilter extends OncePerRequestFilter {
             "/image",
             "/css",
             "/favicon.ico",
+            // "/webjars",
             "/d/druid"
         };
 
